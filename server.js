e2301c0061688773c8012954aaa7ce0466dd28c8
Prajwol Karki <112867720+prajwolkarki@users.noreply.github.com>
2024-11-26 22:48:36 +0545

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://nutritionapp-psi.vercel.app', // Specific origin
-  credentials: true, // Allow credentials
+  origin: 'https://nutritionapp-psi.vercel.app', // Remove the trailing slash
+  credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
