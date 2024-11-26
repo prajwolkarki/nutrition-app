86d19cd502a6e2404411455aa7dbfde30fdc0964
Prajwol Karki <112867720+prajwolkarki@users.noreply.github.com>
2024-11-26 21:14:46 +0545

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 const allowedOrigins = [
     'http://localhost:3000', 
+    'https://nutrition-app-dpud.onrender.com'
 ];
 
 app.use(cors({
